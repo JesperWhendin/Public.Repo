d52cfc902c9b757a3d9032c9687ba0f19987f962
JesperWhendin <143404976+JesperWhendin@users.noreply.github.com>
2023-09-19 17:20:57 +0200

    OOP C# 2023 19-9-23 17:20

---
+++ OOPintro C# 2023/Pokemon.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPintro_C__2023
+{
+    internal class Pokemon
+    {
+        // Dessa variabler är deklarerade med _ i början av namnen för att de
+        // symboliserar fields/fält. De deklareras på det här sättet för att
+        // de finns för hela klassen och inte är lokala varabler.Fält kan också
+        // ha access modifiers* (*AM) (så som public eller private) *AM används
+        // för att skydda variabler så att de inte skall kunna ändras hur som helst.
+
+        public string _name;
+        public string _type;
+        public int _level;
+
+        // Undertill här är en tom konstruktor* (*ctor), den behöver
+        // inte skrivas ut då en klass som har en odefinerad ctor alltid
+        // har en defaultkonstruktor (vilket är en tom ctor).
+
+        public Pokemon() 
+        {
+            
+        }
+
+        // Detta ovan är en klass med en tom ctor, dvs. den kan instansieras utan
+        // att ta in parametrar. Man kan skapa en ny ctor som tvingar intag av parametrar
+        // för att instansiera en ny klass. Exempel nedan där field/fält används.
+
+        public Pokemon(string name, string type, int level) 
+        {
+            _name = name; // Exempel på hur fält används i jämförelse med variabler.
+            _type = type;
+            _level = level;
+        }
+
+        // Ett annat sätt, som verkar säkrare då inte användaren fritt
+        // kan ange namn eller andra parametrar när ett objekt instansieras,
+        // är med så kallade properties. En property är egentligen bara en 
+        // field (fält) och två sammanhängande metoder, get- och set-.
+        // Get-metod är till för att kontrollera hur ett värde presenteras.
+        // Set-metod är till för att kontrollera hur ett värde sätts.
+
+        private string _characterTrait;
+        public string CharacterTrait
+        {
+            get
+            {
+                Console.WriteLine(_name + " has a " + _characterTrait + " behaviour.");
+                return _characterTrait;
+            }
+            //  Get-metoden på rad 51 ser egentligen ut som följande:
+
+            // public string get_CharacterTrait()
+            // {
+            //    return _name;
+            // }
+
+            set
+            {
+                Console.WriteLine(_name + " was given a " + _characterTrait + " behaviour.");
+                _characterTrait = value;
+            }
+            // Set-metoden är på samma sätt som get- också en metod:
+
+            // public void set_CharacterTrait(string value)
+            // {
+            //     _characterTrait = value;
+            // }
+
+            // Den här propertyn låter en nu styra fältet, _characterTrait,
+            // även om det ligger som private.
+        }
+
+        // Här nedan är ytterligare ett sätt att instansiera en Pokemon med fler parametrar.
+        // public Pokemon(string name, string type, int level, string characterTrait)
+        // {
+        //     _name = name;
+        //     _type = type;
+        //     _level = level;
+        //     _characterTrait = characterTrait;
+        // }
+        
+    }
+ }
+
---
+++ OOPintro C# 2023/Program.cs
@@ -0,0 +1,67 @@
+﻿using OOPintro_C__2023;
+using System.Net.Security;
+
+Console.ForegroundColor = ConsoleColor.DarkYellow;
+Console.WriteLine("Intro och grunder inom OOP.");
+Console.ForegroundColor = ConsoleColor.DarkCyan; Console.WriteLine("---------------------------"); Console.ForegroundColor = ConsoleColor.White;
+
+Console.WriteLine("Test av instansiering utan hjälp av konstruktor / ctor");
+
+Pokemon abra = new Pokemon(); // Instansierar ett nytt objekt av klassen/typen Pokemon
+// Denna instansen av klassen/typen är döpt till abra och här deklarerar jag namn, type och level.
+abra._name = "Abra";
+abra._type = "Psychic";
+abra._level = 10;
+
+Console.WriteLine(abra); // Skriver ut "OOPintro_C__2023.Pokemon", vilket är typen på detta objektet.
+// Typen är Pokemon och Pokemon ligger i namespace OOPintro_C__2023
+Console.WriteLine(abra._name); // Skriver ut det deklarerade namnet för abra
+Console.WriteLine(abra._type); // Skriver ut den deklarerade pokeTypen för abra
+Console.WriteLine(abra._level); // Skriver ut den deklarerade leveln för abra
+
+Console.WriteLine(); // Radbrytning.
+
+Pokemon pidgey = new Pokemon(); // Ny instans av klassen/typen Pokemon i form av pidgey
+pidgey._name = "Pidgey";
+pidgey._type = "Flying";
+pidgey._level = 5;
+
+Console.WriteLine(pidgey); // Skriver fortfarande ut "OOPintro_C__2023.Pokemon" då
+// pidgey och abra är instanser av samma typ.
+Console.WriteLine(pidgey._name);
+Console.WriteLine(pidgey._type);
+Console.WriteLine(pidgey._level);
+
+Console.ForegroundColor = ConsoleColor.DarkCyan; Console.WriteLine("---------------------------"); Console.ForegroundColor = ConsoleColor.White;
+
+Console.WriteLine("Test av instansiering med hjälp av ctor.");
+
+Pokemon machop = new Pokemon("Machop", "Fighting", 15);
+// Exempel på hur en klass instansieras med hjälp av en ctor.
+
+Console.WriteLine(machop); // För att belysa ny instans av samma klass/typ.
+Console.WriteLine(machop._name);
+Console.WriteLine(machop._type);
+Console.WriteLine(machop._level);
+// Enbart machop.name går inte då name är en lokal variabel för klassen.
+// Därför använder man sig här av fält (ex: _name) för att beropa detta objektets specifika variabel.
+Console.ForegroundColor = ConsoleColor.DarkCyan; Console.WriteLine("---------------------------"); Console.ForegroundColor = ConsoleColor.White;
+
+Console.WriteLine("Test av instansiering med hjälp av properties.");
+
+Pokemon scyther = new Pokemon("Scyther", "Bug", 20);
+
+Console.WriteLine(scyther); // Belyser ny instans av samma klass/typ - "OOPintro_... "
+Console.WriteLine(scyther._name);
+Console.WriteLine(scyther._type);
+Console.WriteLine(scyther._level);
+// Console.WriteLine(scyther.Level); - Level i det här fallet är en lokal variabel i Pokemon.cs.
+scyther.CharacterTrait = "defensive";
+// scyther._characterTrait = "Aggressive"; - Inaccessible due to protectionlevel (private).
+// Här förändrar vi alltså värdet på _characterTrait, även om det ligger som private.
+// Detta på grund av propertyn och dess get-/set- metoder.
+Console.WriteLine(scyther.CharacterTrait);
+// Console.WriteLine(scyther._characterTrait); - Går heller inte att nå likt exemplet någon rad ovan.
+
+Console.ForegroundColor = ConsoleColor.DarkCyan; Console.WriteLine("---------------------------"); Console.ForegroundColor = ConsoleColor.White;
+
